f48c1857397605d1808e7ebfd2341a83bf52d0dd
Les Lim <liml@hrw.org>
2024-10-16 21:00:45 -0500

    nav: add Esc key listener to close details elements

--- nav/nav.js
+++ nav/nav.js
@@ -1,5 +1,5 @@
 /**
- * Close the navigation modal once a selection has been made.
+ * Close the navigation menu once a selection has been made.
  */
 document.querySelectorAll('#sections-list a').forEach(element => {
   element.addEventListener('click', (event) => {
@@ -8,10 +8,21 @@ document.querySelectorAll('#sections-list a').forEach(element => {
 });
 
 /**
- * Close the language selector if any other click is made.
+ * Close the language selector if any outside click is made.
  */
 document.addEventListener('click', (event) => {
-  if (!event.target.closest('#languages-toggle')) {
+  if (!event.target.closest('#nav-languages')) {
     document.querySelector('#nav-languages').removeAttribute('open');
   }
 });
+
+/**
+ * Close all details elements when Esc key is pressed.
+ */
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    document.querySelector('#nav-sections').removeAttribute('open');
+    document.querySelector('#nav-languages').removeAttribute('open');
+  }
+});
+
